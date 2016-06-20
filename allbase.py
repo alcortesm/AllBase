34b0fc36bd7318a36b990104593d07e4a570fb86
Alberto Cortés <alberto@sourced.tech>
2016-06-20 18:47:15 +0200

    gitignore

--- allbase.py
+++ allbase.py
@@ -5,7 +5,16 @@ parser.add_argument("integer",
                     help="the number you want to show in different bases",
                     type=int)
 args = parser.parse_args()
-print(args.integer)
+
+d = args.integer
+b = bin(d)
+o = oct(d)
+h = hex(d)
+
+output = '{b} {o} {d} {h}'.format(
+    b, o, d, h)
+
+print(output)
 
 
 def main():
