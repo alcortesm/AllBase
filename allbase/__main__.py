f042c2c0a2906d320e00878097730e86821e4f7e
Alberto Cortés <alberto@sourced.tech>
2016-06-23 14:15:24 +0200

    tobase now accepts and returns several numbers

--- allbase/__main__.py
+++ allbase/__main__.py
@@ -2,12 +2,12 @@ import sys
 from allbase import args
 from allbase import tobase
 
-n, bases, ok, err = args.parse(sys.argv[1:])
+nums, bases, ok, err = args.parse(sys.argv[1:])
 if not ok:
     print(err, file=sys.stderr)
     sys.exit(1)
 
-result = tobase.do(n, bases)
+result = tobase.do(nums, bases)
 
 print(result)
 sys.exit(0)
