46102694e1ce67e49d16adf48228e94546817f75
Alberto Cortés <alberto@sourced.tech>
2016-06-23 14:23:04 +0200

    clean main

--- allbase/__main__.py
+++ allbase/__main__.py
@@ -7,8 +7,7 @@ if not ok:
     print(err, file=sys.stderr)
     sys.exit(1)
 
-results = tobase.do(nums, bases)
-for r in results:
-    print(r)
+for line in tobase.do(nums, bases):
+    print(line)
 
 sys.exit(0)
