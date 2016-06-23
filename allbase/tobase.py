664ca8b8a9f71e23a825bcd805e930299648824b
Alberto Cortés <alberto@sourced.tech>
2016-06-23 14:16:57 +0200

    clean tobase

--- allbase/tobase.py
+++ allbase/tobase.py
@@ -1,6 +1,6 @@
 def do(nums, bases):
 
-    """ list of numbers, each of it a list of the number in different bases """
-    nums_in_bases = [[b.to_str(n) for b in bases] for n in nums]
-    """ for each number, join its representation in different bases """
-    return [" ".join(num) for num in nums_in_bases]
+    nums_in_all_bases = [[b.to_str(n) for b in bases] for n in nums]
+    """ for each number, join its representation in different bases
+        in a single string using spaces """
+    return [" ".join(num) for num in nums_in_all_bases]
